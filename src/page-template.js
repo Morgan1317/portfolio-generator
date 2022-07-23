85896a6b86d43cf2ad7b6d80280fefc6c3cb0e39
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-07-23 17:19:04 -0500

    created new html page

---
+++ src/page-template.js
@@ -0,0 +1,22 @@
+// In this function, which has no parameters, we need parentheses to hold the place where parameters would've been.
+//  return statement is implied when there is only a single statement
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+
+  module.exports = generatePage; 
